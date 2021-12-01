7d402efc45f01dabe3bdb52bc6d897a46e5209ba
Pyxless <95367189+Pyxless@users.noreply.github.com>
2021-12-02 00:29:07 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@ $email = $_POST['user_email'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.com.';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com.';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'woman.belarusian@gmail.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'Nikita2002135790'; // Ваш пароль от почты с которой будут отправляться письма
@@ -35,6 +35,6 @@ $mail->AltBody = '';
 if(!$mail->send()) {
     echo 'Error';
 } else {
-    header('location: Главная.html');
+    header('location: index.html');
 }
 ?>
