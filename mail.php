7333c7ab70528f20002026625ccceb3722ae8593
Pyxless <95367189+Pyxless@users.noreply.github.com>
2021-12-02 00:34:53 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -11,15 +11,15 @@ $email = $_POST['user_email'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com.';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.com.';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'woman.belarusian@gmail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Username = 'womanbelarusian@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'Nikita2002135790'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('woman.belarusian@gmail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('n.birkos1@gmail.com');     // Кому будет уходить письмо 
+$mail->setFrom('womanbelarusian@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('woman.belarusian@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
